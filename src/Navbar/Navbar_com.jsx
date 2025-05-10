e3354215435bfde78a545ceb286bc57a0a2f781c
shivavarun09 <varunapps09@gmail.com>
2025-05-10 15:38:20 +0530

    msg

--- src/Navbar/Navbar_com.jsx
+++ src/Navbar/Navbar_com.jsx
@@ -2,23 +2,22 @@ import React, { Component } from 'react'
 import {Container,Navbar,Nav} from 'react-bootstrap'
 import './Navbar_com.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { RiLoginBoxFill } from "react-icons/ri";
+import {Link} from 'react-router-dom'
 export default class Navbar_com extends Component {
   render() {
     return (
-      <div >
-         <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">React this.state examples</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link className='navlinks' href="/count-bgchange">Count Bgchange</Nav.Link>
-            <Nav.Link className='navlinks' href="/random-color">Random Color</Nav.Link>
+      <div className='navbar'>
+        
+            <Link className='links' to='/count-bgchange'><span>Count Bgchange</span></Link >
+            <Link className='links' to='/random-color'><span>Random Color</span></Link >
+            <Link className='links' to='/random-quote'><span>Random Quote</span></Link >
+            <Link className='links' to='/login'><span>Login&SignUp</span></Link >
+            <Link className='links' to='/togglebg'><span>Light&Dark mode</span></Link >
+            {/* <Nav.Link className='navlinks' href="/random-color">Random Color</Nav.Link>
             <Nav.Link className='navlinks' href="/random-quote">Random Quote</Nav.Link>
             <Nav.Link className='navlinks' href="/login">Login&SignUp</Nav.Link>
-            <Nav.Link className='navlinks' href="/togglebg">Light&Dark mode</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+            <Nav.Link className='navlinks' href="/togglebg">Light&Dark mode</Nav.Link> */}
+        
       </div>
     )
   }
