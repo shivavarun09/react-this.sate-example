ec6e2a0e63dacf86f62322a4d42011803accf766
shivavarun09 <varunapps09@gmail.com>
2025-05-10 15:11:00 +0530

    first commit

--- src/Randomclr/RandomClr.jsx
+++ src/Randomclr/RandomClr.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import {Button} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './RandomClr.css'
+import './Randomclr.css'
 export default class Home extends Component {
   constructor(){
     super();
